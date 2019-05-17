5692763b3fd15f8a606a2b589509698e321dca4c
texmexryan <rjohns91@msn.com>
2019-05-17 10:46:47 -0700

    added packages and folders

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 
 class App extends Component {
@@ -14,7 +13,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
         </p>
